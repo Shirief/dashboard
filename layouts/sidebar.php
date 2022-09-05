fc3d22aa864d032d942cc3a3431f3b17841d0899
RhannelD <rhanneldinlasan@gmail.com>
2022-09-05 20:11:31 +0800

    init dashboard

---
+++ layouts/sidebar.php
@@ -0,0 +1,50 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+    <ul class="sidebar-nav" id="sidebar-nav">
+        <li class="nav-item">
+            <a class="nav-link collapsed">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed">
+                <i class="bi bi-bell"></i>
+                <span>Notify</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-archive"></i>
+                <span>Archive</span>
+                <i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="">
+                        <i class="bi bi-circle"></i>
+                        <span>View the Pending Request Forms</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="bi bi-circle"></i>
+                        <span>View the Approve Request Forms</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="bi bi-circle"></i>
+                        <span>View the Location of The Vehicle</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="bi bi-circle"></i>
+                        <span>View the Content of the Vehicle</span>
+                    </a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
