d1e6451b7b53d3cd8893e20fd298b07b2a01018e
RhannelD <rhanneldinlasan@gmail.com>
2022-09-05 20:55:19 +0800

    add compose to sidebar

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -2,6 +2,33 @@
 <aside id="sidebar" class="sidebar">
     <ul class="sidebar-nav" id="sidebar-nav">
         <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#compose-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-pencil-square"></i>
+                <span>Compose</span>
+                <i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="compose-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="">
+                        <i class="bi bi-circle"></i>
+                        <span>Work Order</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="bi bi-circle"></i>
+                        <span>Travel Order</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="bi bi-circle"></i>
+                        <span>Trip Ticket</span>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="nav-item">
             <a class="nav-link collapsed">
                 <i class="bi bi-grid"></i>
                 <span>Dashboard</span>
@@ -14,12 +41,12 @@
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+            <a class="nav-link collapsed" data-bs-target="#archive-nav" data-bs-toggle="collapse" href="#">
                 <i class="bi bi-archive"></i>
                 <span>Archive</span>
                 <i class="bi bi-chevron-down ms-auto"></i>
             </a>
-            <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <ul id="archive-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                 <li>
                     <a href="">
                         <i class="bi bi-circle"></i>
